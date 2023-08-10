aae6cbad06bf08fd733b0ae2baa2f720860b85d6
Henrik Løvold <henrik.lovold@gmail.com>
2023-08-10 15:39:43 +0200

    Baseline iterative added

---
+++ henrik_code/baseline_iterative.py
@@ -0,0 +1,21 @@
+
+
+def collatz_next(x):
+	if x % 2 == 0:
+		return x//2
+	else:
+		return x*3 + 1
+
+def collatz(x):
+	path_len = 0
+	next_x = collatz_next(x)
+	while next_x != 1:
+		path_len += 1
+		next_x = collatz_next(next_x)
+	return path_len + 1
+
+
+if __name__ == "__main__":
+	N = 1_000_000 # I wouldn't dare run this with 1*10**7
+	for i in range(2, N):
+		collatz(i)
