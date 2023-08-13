0df1a0b9da87b55d4c44b3729bce7d4fbe35615a
larstvei <larstvei@ifi.uio.no>
2023-08-13 19:32:30 +0200

    Add rust versions
    A decent recursive version and a disappointing iterative version. The recursive
    solution could easily be made parallel.
    
    For N = 10^9:
    
    ```
    λ time cargo run --release --bin iterative --quiet
    987
    
    ________________________________________________________
    Executed in   13.19 secs    fish           external
       usr time   12.89 secs    0.09 millis   12.89 secs
       sys time    0.24 secs    2.47 millis    0.24 secs
    ```
    
    ```
    λ time cargo run --release --bin recursive --quiet
    987
    
    ________________________________________________________
    Executed in    9.99 secs    fish           external
       usr time    9.48 secs    0.08 millis    9.48 secs
       sys time    0.23 secs    3.06 millis    0.23 secs
    ```
    
    ```
    λ time cargo run --release --bin parallel --quiet
    987
    
    ________________________________________________________
    Executed in    2.38 secs    fish           external
       usr time   10.34 secs    0.11 millis   10.34 secs
       sys time    0.27 secs    2.97 millis    0.27 secs
    ```

---
+++ larstvei/src/parallel.rs
@@ -0,0 +1,32 @@
+use rayon::prelude::*;
+use std::sync::atomic::Ordering;
+use std::sync::atomic::AtomicU16;
+
+fn next_collatz(n: usize) -> usize {
+    if n % 2 == 0 {
+        n / 2
+    } else {
+        3 * n + 1
+    }
+}
+
+fn collatz_rec(n: usize, memo: &[AtomicU16]) -> u16 {
+    match memo.get(n).map(|x| x.load(Ordering::Relaxed)) {
+        Some(0) => {
+            let res = 1 + collatz_rec(next_collatz(n), memo);
+            memo[n].store(res, Ordering::Relaxed);
+            res
+        }
+        Some(y) => y,
+        None => 1 + collatz_rec(next_collatz(n), memo),
+    }
+}
+
+fn main() {
+    let n = 1E9 as usize;
+    let memo: Vec<AtomicU16> = (0..n).map(|_| AtomicU16::new(0)).collect();
+    memo[1].store(1, Ordering::Relaxed);
+    (1..=n).into_par_iter().for_each(|x| { collatz_rec(x, &memo); });
+    let res = memo.iter().map(|x| x.load(Ordering::Relaxed)).max().unwrap();
+    println!("{}", res);
+}
