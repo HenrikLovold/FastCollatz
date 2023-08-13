1d6e7c19f766fdc02bfacf18e9d855603ef79cb5
larstvei <larstvei@ifi.uio.no>
2023-08-13 20:43:56 +0200

    Make an interface for the parallel version

--- larstvei/src/parallel.rs
+++ larstvei/src/parallel.rs
@@ -1,3 +1,4 @@
+use argparse::{ArgumentParser, Store};
 use rayon::prelude::*;
 use std::sync::atomic::Ordering;
 use std::sync::atomic::AtomicU16;
@@ -23,8 +24,16 @@ fn collatz_rec(n: usize, memo: &[AtomicU16]) -> u16 {
 }
 
 fn main() {
-    let n = 1E9 as usize;
-    let memo: Vec<AtomicU16> = (0..n).map(|_| AtomicU16::new(0)).collect();
+    let mut n = 1E9 as usize;
+
+    {
+        let mut parser = ArgumentParser::new();
+        parser.set_description("Longest Collatz sequence starting below n");
+        parser.refer(&mut n).add_argument("n", Store, "Value of n");
+        parser.parse_args_or_exit();
+    }
+
+    let memo: Vec<AtomicU16> = (0..=n).map(|_| AtomicU16::new(0)).collect();
     memo[1].store(1, Ordering::Relaxed);
     (1..=n).into_par_iter().for_each(|x| { collatz_rec(x, &memo); });
     let res = memo.iter().map(|x| x.load(Ordering::Relaxed)).max().unwrap();
